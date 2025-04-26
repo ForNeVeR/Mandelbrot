cac59ec3e66d742d2a0c9cf3a61f2c1fb380e8d0
Friedrich von Never <friedrich@fornever.me>
2025-04-27 01:01:49 +0200

    Rust: parse the arguments

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,35 @@
-// SPDX-FileCopyrightText: 2025 Friedrich von Never <friedrich@fornever.me>
+// SPDX-FileCopyrightText: 2010-2025 Friedrich von Never <friedrich@fornever.me>
 //
 // SPDX-License-Identifier: MIT
 
 extern crate sdl2;
 
-fn main() {
-    let sdl_context = sdl2::init().unwrap();
+use crate::Mode::{Fullscreen, Windowed};
+
+const DEFAULT_VIDEO_WIDTH: i32 = 400;
+const DEFAULT_VIDEO_HEIGHT: i32 = 400;
+
+#[derive(Debug)]
+enum Mode {
+    Windowed(i32, i32),
+    Fullscreen,
+}
+
+fn main() -> Result<(), String> {
+    let sdl_context = sdl2::init()?;
+
+    let args: Vec<String> = std::env::args().collect();
+    let mode = match args.len() {
+        1 => Windowed(DEFAULT_VIDEO_WIDTH, DEFAULT_VIDEO_HEIGHT),
+        2 if args[1] == "--full" => Fullscreen,
+        3 => Windowed(
+            args[1].parse().map_err(|e| format!("Invalid width: {}", e))?,
+            args[2].parse().map_err(|e| format!("Invalid height: {}", e))?,
+        ),
+        _ => return Err(String::from("Usage: Mandelbrot [W H] | [--full]"))
+    };
+
+    println!("TODO: Initialize SDL with mode: {:?}", mode);
+    
+    Ok(())
 }
